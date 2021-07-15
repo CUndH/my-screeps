2ac8b4619c8c7101d22b97fdb2c629952dbac643
陈元皓 <joeycairo@xingren.com>
2021-07-15 10:17:41 +0800

    抽象出creep的工作

---
+++ src/CreepWork.js
@@ -0,0 +1,31 @@
+export default class CreepWork {
+  static harvest(creep, target) {
+    if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(target)
+    };
+  }
+
+  static upgrade(creep, controller) {
+    if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(controller);
+    }
+  }
+
+  static build(creep, constructionSite) {
+    if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(constructionSite);
+    }
+  }
+
+  static withDraw(creep, target, source) {
+    if (creep.withdraw(target, source) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(target);
+    }
+  }
+
+  static transfer(creep, target, source) {
+    if (creep.transfer(target, source) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(target);
+    }
+  }
+}
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,14 +1,12 @@
+import CreepWork from "./CreepWork";
+
 const roleHarvester = (creep) => {
   if (creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()) {
     let target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-    if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(target);
-    }
+    CreepWork.harvest(creep, target);
   } else {
     let target = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
-    if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(target);
-    }
+    CreepWork.transfer(creep, target, RESOURCE_ENERGY);
   }
 }
 
