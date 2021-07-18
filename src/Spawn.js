3d84fe2d527405f134abff7fac880fc28bb54dc9
CUndH <875021528@qq.com>
2021-07-18 14:27:30 +0800

    优化创造逻辑的creep创造顺序问题

--- src/Spawn.js
+++ src/Spawn.js
@@ -1,5 +1,34 @@
+import { minumMap } from '../constant/index';
 export default class Spawn {
-  static spawnCreep (target, body, name, memory) {
+  static spawn(target, body, name, memory) {
     target.spawnCreep(body, name, memory);
+  }
+
+  static needSpawnCreep(role) {
+    const creepNumber = _.sum(Game.creeps, (c) => c.memory.role == role);
+    if (creepNumber < minumMap[role]) {
+      return true;
+    }
+    return false;
+  }
+
+  static spawnCreep (room) {
+    if (this.needSpawnCreep('harvester')) {
+      this.spawn(room, [WORK, CARRY, MOVE], `harvester_${Game.time}`, {
+        memory: {role: 'harvester', working: false}
+      });
+    } else if (this.needSpawnCreep('upgrader')) {
+      this.spawn(room, [WORK, CARRY, MOVE], `upgrader_${Game.time}`, {
+        memory: {role: 'upgrader', working: false}
+      });
+    } else if (this.needSpawnCreep('builder')) {
+      this.spawn(room, [WORK, CARRY, MOVE], `builder_${Game.time}`, {
+        memory: {role: 'builder', working: false}
+      });
+    } else if (this.needSpawnCreep('wallRepairer')) {
+      this.spawn(room, [WORK, CARRY, MOVE], `wallRepairer_${Game.time}`, {
+        memory: {role: 'wallRepairer', working: false}
+      });
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,11 @@
 import { errorMapper } from '../modules/errorMapper';
 import {roleHarvester, roleBuilder, roleUpgrader, roleWallRepairer} from './roles/index';
 import Spawn from './spawn';
-import { minumMap } from '../constant/index';
 
 export const loop = errorMapper(() => {
 
-  for (let role in minumMap) {
-    const creepsNumber = _.sum(Game.creeps, (c) => c.memory.role == role);
-    for (let name in Memory.creeps) {
-      // creep 死亡后清除内存
-      if (!Game.creeps[name]) {
-        delete Memory.creeps[name];
-      }
-      // creep 低于限定值后重新创造相应角色
-      if (creepsNumber < minumMap[role]) {
-        Spawn.spawnCreep(Game.spawns['Spawn1'], [WORK, CARRY, MOVE], `${role}_${Game.time}`, {
-          memory: {
-            role,
-            working: false,
-          }
-        });
-      }
-    }
-  }
+  // 创造creep
+  Spawn.spawnCreep(Game.spawns['Spawn1']);
 
   for (let name in Game.creeps) {
     let creep = Game.creeps[name];
